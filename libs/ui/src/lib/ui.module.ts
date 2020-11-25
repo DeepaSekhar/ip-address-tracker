f8b4f7f84e5e979abcde8597faaccdb583ec152e
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-11-25 22:32:31 +0000

    created a search component and added to the app component

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [NavbarComponent],
-  exports: [NavbarComponent],
+  declarations: [NavbarComponent, SearchComponent],
+  exports: [NavbarComponent, SearchComponent],
 })
 export class UiModule {}
