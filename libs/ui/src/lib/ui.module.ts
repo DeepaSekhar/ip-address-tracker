a09f192dfa6c9418313db596ac81882332734e5b
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-11-25 22:34:52 +0000

    created a info component and map component to the Ui

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchComponent } from './search/search.component';
+import { InfoComponent } from './info/info.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [NavbarComponent, SearchComponent],
-  exports: [NavbarComponent, SearchComponent],
+  declarations: [NavbarComponent, SearchComponent, InfoComponent, MapComponent],
+  exports: [NavbarComponent, SearchComponent, InfoComponent, MapComponent],
 })
 export class UiModule {}
